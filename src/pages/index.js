d56a535fe3573b82007eeda24b414cef11d00940
Baraka Mahili <georgesmahili@gmail.com>
2018-09-03 19:31:20 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Netlify and Coders!</h1>
+    <p>My name is Baraka Mahili.</p>
+    <p>First time to use Gatsby, for real.</p>
+    <a target="_blank" href="https://gbmahili.github.io/Baraka_Mahili.me/">Check out my page.</a>
+  </div>
+)
+
+export default IndexPage
